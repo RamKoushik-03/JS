12d9fbcfd5473d439268ffe3ad741a8bff9783c2
Ram Koushik Mendru <140931894+RamKoushik-03@users.noreply.github.com>
2025-07-05 09:16:02 +0000

    Commit2

--- g1.js
+++ g1.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+const y = 20;
+let x = 10;
+var z = 30;
+//Dont use var in most cases , since it has function scope
+//Use let and const instead
+
+p = 20;
+//It will work, but it is not recommended to use undeclared variables
+
+let gg;
+//This varible value is undefined, since it is not initialized
+
+console.table([x, y, z, gg]);
+
+
+
