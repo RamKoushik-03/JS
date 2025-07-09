c72a699feb030ff39db5cd9d5d44c4243ca66d68
Ram Koushik Mendru <140931894+RamKoushik-03@users.noreply.github.com>
2025-07-09 13:30:48 +0000

    commit12

--- d3.js
+++ d3.js
@@ -5,11 +5,13 @@ console.log(a.length); // 5
 console.log(a[a.length - 1]); // 5
 console.log(a[10]); // undefined
 console.log(a[-1]); // undefined (negative indices are not supported in JavaScript arrays)
+
 // Adding elements
 a.push(6); // Adds 6 to the end of the array
 console.log(a); // [1, 2, 3, 4, 5, 6]
 a.unshift(0); // Adds 0 to the beginning of the array
 console.log(a); // [0, 1, 2, 3, 4, 5, 6]
+
 // Removing elements
 a.pop(); // Removes the last element (6)
 console.log(a); // [0, 1, 2, 3, 4, 5]
@@ -22,3 +24,7 @@ console.log(b); // [2, 3, 4]
 a.splice(1, 2, 10, 20); // Removes 2 elements
 console.log(a); // [1, 10, 20, 4, 5]
 
+console.log(a.includes(10)); // true
+console.log(a.indexOf(20)); // 2
+console.log(a.lastIndexOf(20)); // 2
+console.log(a.join(', ')); // "1, 10, 20, 4, 5"
