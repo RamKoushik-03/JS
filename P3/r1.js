61645870951d8df07698eed72350dd2afc915441
Ram Koushik Mendru <140931894+RamKoushik-03@users.noreply.github.com>
2025-07-23 16:37:43 +0000

    commit27

--- P3/r1.js
+++ P3/r1.js
@@ -2,9 +2,19 @@ const promiseOne = new Promise(function(resolve, reject) {
 
     setTimeout(function() {
         console.log('Async task is completed');
+        resolve()
     }, 1000) 
 })
 
 promiseOne.then(function() {
     console.log("Promise consumned");
+
+})
+
+new Promise(function(resolve, reject) {
+    setTimeout(function () {
+        console.log("Async task 2");
+    }, 1000)
+}).then(function() {
+    console.log("Async 2 resolved");
 })
