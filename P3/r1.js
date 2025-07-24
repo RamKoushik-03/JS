2d94b56aad988140ea75877e05e0aa957414af59
Ram Koushik Mendru <140931894+RamKoushik-03@users.noreply.github.com>
2025-07-24 22:45:13 +0530

    Update r1.js

--- P3/r1.js
+++ P3/r1.js
@@ -18,3 +18,24 @@ new Promise(function(resolve, reject) {
 }).then(function() {
     console.log("Async 2 resolved");
 })
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Ash", email: "Ash@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hero", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
