374a035ac81bd57ef4b0bf812cd1bf45b7151462
Ram Koushik Mendru <140931894+RamKoushik-03@users.noreply.github.com>
2025-07-26 20:22:37 +0530

    Update r2.js

--- P3/r2.js
+++ P3/r2.js
@@ -13,3 +13,21 @@ const user = {
 
 console.log(user.userName);
 console.log(user.getUserDetails());
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("ram", 09, true)
+const userTwo = new User("Ash", 07, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
